4e8c2917a75e4e0d41dd190bb640264de70fded2
Arnaldo Perez <arnaldoeperezb@gmail.com>
2024-03-21 13:15:10 -0400

    testing dev container

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,2 +1,10 @@
 #!/bin/bash 
-node server.js
+
+clear
+echo "Your public url is:"
+echo "------------------------------------------"
+echo "http://$CODESPACE_NAME-3001.app.github.dev"
+echo "------------------------------------------"
+
+nohup node server.js
+
